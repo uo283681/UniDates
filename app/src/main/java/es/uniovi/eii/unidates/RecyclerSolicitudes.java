bddaad1575175c90f8a5284d864572c17e741f7a
uo283681 <uo283681@uniovi.es>
2022-11-19 10:17:24 +0100

    first commit

--- app/src/main/java/es/uniovi/eii/unidates/RecyclerSolicitudes.java
+++ app/src/main/java/es/uniovi/eii/unidates/RecyclerSolicitudes.java
@@ -9,6 +9,6 @@ public class RecyclerSolicitudes extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.recycler_solicitudes);
     }
 }
